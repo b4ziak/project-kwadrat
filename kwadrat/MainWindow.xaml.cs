9d9b42ae03941c70e86b5f889a11e7cf6a1edeb6
b4ziak <baziakkacper@gmail.com>
2024-09-30 19:19:22 +0200

    First commit of whole project

---
+++ kwadrat/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using kwadrat.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kwadrat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Kwadrat kwadrat;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.kwadrat = new Kwadrat(rectWrap, 0, (SolidColorBrush)new BrushConverter().ConvertFromString("black"), 0, txtPole, txtObwod, lblTest, opacitySlider);
+        }
+
+
+        private void clearText()
+        {
+            txtBok.Text = String.Empty;
+            txtObwod.Text = String.Empty;
+            txtPole.Text = String.Empty;
+            lblKomunikat.Content = String.Empty;
+            rectWrap.Points.Clear();
+        }
+        private void clearAll()
+        {
+            clearText();
+            rectWrap.Points.Clear();
+        }
+
+        private void txtBok_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double bok;
+            if (double.TryParse(txtBok.Text, out bok) && bok >= 0 && bok <= 400)
+            {
+                lblKomunikat.Content = String.Empty;
+                this.kwadrat?.setSize(bok);
+                this.kwadrat?.showParams();
+            }
+            else
+            {
+                lblKomunikat.Content = "Wpisz liczbę dodatnią i mniejsza od 400";
+            }
+        }
+
+        private void btnWyczysc_Click(object sender, RoutedEventArgs e)
+        {
+            clearAll();
+        }
+
+        private void btnRysuj_Click(object sender, RoutedEventArgs e)
+        {
+            double bok;
+            if (double.TryParse(txtBok.Text, out bok) && bok >= 0 && bok <= 400)
+            {
+                SolidColorBrush color = (SolidColorBrush)new BrushConverter().ConvertFromString(cmbKolory.Text);
+                double opacity = (cbPrzezroczysty.IsChecked.Value) ? 0.5 : 1;
+                this.kwadrat?.setColor(color);
+                this.kwadrat?.setOpacity(opacity);
+                this.kwadrat?.draw();
+                this.kwadrat?.showParams();
+            }
+            else
+            {
+                lblKomunikat.Content = "Brak danych lub zły rozmiar bokud";
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            double bok;
+            if (double.TryParse(txtBok.Text, out bok) && bok >= 0 && bok < 400)
+            {
+                bok++;
+                clearAll();
+                txtBok.Text = bok.ToString();
+                this.kwadrat?.drawSize(bok);
+                this.kwadrat?.showParams();
+            }
+            else
+            {
+                lblKomunikat.Content = "ZA DUŻO KLIKASZ NA PLUS";
+            }
+        }
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            double bok;
+            if (double.TryParse(txtBok.Text, out bok) && bok > 0 && bok <= 400)
+            {
+                bok--;
+                clearAll();
+                txtBok.Text = bok.ToString();
+                this.kwadrat?.drawSize(bok);
+                this.kwadrat?.showParams();
+            }
+            else
+            {
+                lblKomunikat.Content = "ZA DUŻO KLIKASZ NA MINUS";
+            }
+        }
+
+        private void radioPokaz_Checked(object sender, RoutedEventArgs e)
+        {
+            kwadrat?.show(true);
+        }
+        private void radioUkryj_Checked(object sender, RoutedEventArgs e)
+        {
+            kwadrat?.show(false);
+        }
+
+        private void btnObrot_Click(object sender, RoutedEventArgs e)
+        {
+            kwadrat?.rotateRight(10);
+        }
+
+        private void btnZmniejsz_Click(object sender, RoutedEventArgs e)
+        {
+            kwadrat.cornerTransform(15);
+            this.kwadrat?.showParams();
+        }
+
+        private void slide_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double opacity = opacitySlider.Value / 100;
+            kwadrat?.setOpacity(opacity);
+        }
+    }
+}
---
+++ kwadrat/Models/Kwadrat.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace kwadrat.Models
+{
+    internal class Kwadrat
+    {
+        protected System.Windows.Shapes.Polygon poly;
+        protected System.Windows.Point topleft;
+        protected System.Windows.Point topright;
+        protected System.Windows.Point bottomleft;
+        protected System.Windows.Point bottomright;
+        protected SolidColorBrush color;
+        protected double opacity;
+        protected double opacityOldValue;
+        protected double bok;
+        protected int rotation;
+        protected double cornerOffsetPercent;
+        protected int cornerStepDirection;
+        protected int cornerRotation;
+        protected TextBox txtPole;
+        protected TextBox txtObwod;
+        protected System.Windows.Controls.Label lblTest;
+        protected Slider opacitySlider;
+        protected double topWidth;
+        protected double bottomWidth;
+
+        public Kwadrat(
+                System.Windows.Shapes.Polygon poly,
+                double bok,
+                SolidColorBrush color,
+                double opacity,
+                TextBox pole,
+                TextBox obwod,
+                System.Windows.Controls.Label lblTest,
+                Slider slider
+
+               )
+        {
+            this.poly = poly;
+            this.bok = bok;
+            this.color = color;
+            this.opacity = opacity;
+            this.rotation = 0;
+            this.cornerOffsetPercent = 0;
+            this.cornerStepDirection = 1;
+            this.cornerRotation = 1;
+            this.txtObwod = obwod;
+            this.txtPole = pole;
+            this.lblTest = lblTest;
+            this.opacitySlider = slider;
+        }
+
+        public void draw()
+        {
+            this.poly.Points.Clear();
+            this.poly.Stroke = color;
+            this.poly.Fill = color;
+            this.poly.Opacity = opacity;
+            this.poly.Width = bok;
+            this.poly.Height = bok;
+            this.drawSize(this.bok);
+        }
+
+        public void setSize(double bok)
+        {
+            this.bok = bok;
+            this.topWidth = bok;
+            this.bottomWidth = bok;
+
+            this.poly.Width = bok;
+            this.poly.Height = bok;
+        }
+
+        public void setOpacity(double opacity)
+        {
+            this.opacity = opacity;
+            this.opacitySlider.Value = opacity * 100;
+        }
+
+        public void setColor(SolidColorBrush color)
+        {
+            this.color = color;
+        }
+
+        public void show(bool show)
+        {
+            if(!show)
+            {
+                this.opacityOldValue = this.opacity;
+            }
+            this.poly.Opacity = show ? this.opacityOldValue : 0;
+        }
+
+        public void rotateRight(int rotation)
+        {
+            this.rotation += rotation;
+            if(this.rotation >= 360)
+            {
+                this.rotation = 0;
+            }
+
+            lblTest.Content = this.rotation.ToString();
+            RotateTransform rt = new RotateTransform(this.rotation);
+            poly.RenderTransform = rt;
+        }
+
+        public void drawSize(double bok)
+        {
+            double cornerOffset = this.bok * (this.cornerOffsetPercent / 100) / 2;
+
+            if (this.cornerRotation > 0)
+            {
+                this.topleft = new System.Windows.Point(cornerOffset, 0);
+                this.topright = new System.Windows.Point(bok - cornerOffset, 0);
+                this.bottomleft = new System.Windows.Point(bok, bok);
+                this.bottomright = new System.Windows.Point(0, bok);
+
+                this.topWidth = bok - (2 * cornerOffset);
+                this.bottomWidth = bok;
+            }
+            else if (this.cornerRotation < 0)
+            {
+                this.topright = new System.Windows.Point(0, 0);
+                this.topleft = new System.Windows.Point(bok, 0);
+                this.bottomright = new System.Windows.Point(bok - cornerOffset, bok);
+                this.bottomleft = new System.Windows.Point(cornerOffset, bok);
+
+                this.topWidth = bok;
+                this.bottomWidth = bok - (2 * cornerOffset);
+            }
+
+
+            PointCollection polyPoints = new PointCollection();
+            polyPoints.Add(this.topleft);
+            polyPoints.Add(this.topright);
+            polyPoints.Add(this.bottomleft);
+            polyPoints.Add(this.bottomright);
+
+            this.poly.Points = polyPoints;
+        }
+
+        public void cornerTransform(int percent)
+        {
+            this.poly.Points.Clear();
+
+            this.cornerOffsetPercent += percent * this.cornerStepDirection;
+
+            if(this.cornerOffsetPercent >= 100)
+            {
+                this.cornerStepDirection = -1;
+                this.cornerOffsetPercent = 100;
+            }
+
+            if(this.cornerOffsetPercent <= 0)
+            {
+                this.cornerStepDirection = 1;
+                this.cornerRotation = -this.cornerRotation;
+                this.cornerOffsetPercent = 0;
+            }
+
+            lblTest.Content = this.cornerOffsetPercent.ToString();
+
+            this.drawSize(this.bok);
+        }
+
+        public void showPole()
+        {
+            this.txtPole.Text = this.getPole().ToString();
+        }
+
+        public double getPole()
+        {
+            double pole = ((this.topWidth + this.bottomWidth) * this.bok) / 2;
+            return pole;
+        }
+
+        public void showObwod()
+        {
+            txtObwod.Text = this.getObwod().ToString();
+        }
+
+        public double getObwod()
+        {
+            double przyporostokatna = this.bok;
+            double podstawaTrojkata = Math.Abs(this.topWidth - this.bottomWidth);
+            double ramie = Math.Sqrt(Math.Pow(podstawaTrojkata, 2) + Math.Pow(przyporostokatna, 2));
+            double obwod = this.topWidth + this.bottomWidth + ramie * 2;
+
+            return obwod;
+        }
+
+        public void showParams()
+        {
+            this.showObwod();
+            this.showPole();
+        }
+    }
+}
